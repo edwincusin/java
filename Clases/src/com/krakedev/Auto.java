0f6a2e7f3f73692fdb4bf8c686c9453f1d93e731
EDWINCSN <c.edwin32@hotmail.com>
2025-11-02 13:06:35 -0500

    ENCAPSULADO CLASE AUTO || OK

--- Clases/src/com/krakedev/Auto.java
+++ Clases/src/com/krakedev/Auto.java
@@ -1,7 +1,28 @@
 package com.krakedev;
 
 public class Auto {
-	String marca;
-	int anio;
-	float precio;
+	private String marca;
+	private int anio;
+	private float precio;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
 }
