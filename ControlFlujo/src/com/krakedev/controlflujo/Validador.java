0755671f4f4b05a06e09fd331e5e607b1dab61ff
EDWINCSN <c.edwin32@hotmail.com>
2025-11-05 21:18:14 -0500

    EPLICACION SENTENCIA IF, ELSE, ELSE IF

---
+++ ControlFlujo/src/com/krakedev/controlflujo/Validador.java
@@ -0,0 +1,16 @@
+package com.krakedev.controlflujo;
+
+public class Validador {
+
+	public void validarEdad(int edad) {
+		if (edad>=18) {
+			System.out.println("es mayor de edad");
+		}else if(edad>=12 && edad<18) {
+			System.out.println("ES adolecente");
+		}else if(edad>0 && edad <12) {
+			System.out.println("ES NIÑO");
+		}else {
+			System.out.println("EDAD INCORRECTA");
+		}
+	}
+}
