74b838b42e2da439d9cdaf41b56fbc45b29bc6bb
EDWINCSN <c.edwin32@hotmail.com>
2025-11-21 20:55:36 -0500

    RETO 30 || LOG4J Y MAVEN

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,14 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	
+	//METODO SUMAR
+	public static int sumar(int numero1, int numero2) {
+		return numero1+numero2;
+	}
+	
+	//METODO RESTAR PARA INSTANCIAR
+	public int restar(int num1, int num2) {
+		return num1-num2;
+	}
+}
---
+++ ejemplojunit/src/test/java/com/krakedev/ejemplojunit/AppTest.java
@@ -0,0 +1,26 @@
+package com.krakedev.ejemplojunit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testSumar() {
+    	int resultado=Calculadora.sumar(5,10);
+    	assertTrue(resultado==15);
+    }
+    
+    @Test
+    public void testRestar() {
+    	Calculadora c=new Calculadora();
+    	int resultado=c.restar(20, 5);
+    	assertTrue(resultado==1);
+    }
+
+}
